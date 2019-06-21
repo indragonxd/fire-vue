acde4bea7b0a200fa071d6161f5576a277c854bb
sherwin <sherwin.marquez@unmsm.edu.pe>
2019-06-21 14:44:09 -0500

    Operacion CRUD

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
               <h3>Add a new Website</h3>
             </div>
             <div class="card-body">
-              <form @submit.prevent = 'addWeb'>
+              <form @submit.prevent = 'enviar'>
                 <div class="form-group">
                   <input type="text" class="form-control" v-model="newWebSite.name" placeholder="Name">
                 </div>
@@ -43,22 +43,31 @@
                         </tr>
                       </thead>
                       <tbody>
-                        <tr v-for="web of websites" :key="web['.key']">
+                        <tr v-for="web in websites" :key="web['.key']">
                             <td>
                               <a :ref='web.url' target="_blank">{{web.name}}</a>
                             </td>
                             <td>
-                              {{web.autor}}
+                              {{web.autor}} - {{web['.key']}}
                             </td>
                             <td>
-                              <button class="btn btn-danger">Delete</button>
+                              <div class="container">
+                                  <div class="row">
+                                    <div class="col-6">
+                                      <button class="btn btn-danger" @click ='elimarWeb(web)'>Delete</button>
+                                    </div>
+                                    <div class="col-6">
+                                      <button class="btn btn-danger" @click ='editarWeb(web)'>Update</button>
+                                    </div>
+                                  </div>
+                              </div>
+                              
                             </td>
 
                         </tr>
                       </tbody>
                        
                   </table>
-                  <button @click="mostrar"> asda</button>
               </div>
             </div>
         </div>
@@ -81,26 +90,86 @@ export default {
   data(){
     return {
       newWebSite : {
+        '.key' : '',
         name : '',
         autor : '',
         url : ''
       },
+      websites : [],
+      editar : false
     }
-  },
+  },/*
   firebase : {
     //websites es un arreglo de los documentos
       websites:websiteRef
     
+  },*/
+  created(){
+    websiteRef.on('value', snapchat => {
+      //console.log(snapchat.val());
+     
+      //guardar los objetos con sus codigos
+      this.websites = [];
+      let valores = snapchat.val ();
+      for( let val in valores ){
+        this.websites.push({
+          '.key' :val,
+          name : valores[val].name,
+          autor : valores[val].autor,
+          url : valores[val].url
+
+        });
+      }
+      
+    });
   },
   methods : {
+    enviar(){
+      if(this.editar){
+        console.log('websites/'+this.newWebSite['.key']);
+        let actualizado = this.newWebSite;
+
+        db.ref('websites/'+this.newWebSite['.key']).update({
+            name : actualizado.name,
+            autor : actualizado.autor,
+            url  : actualizado.url
+
+        });
+      }else{
+        console.log('guarde');
+        this.addWeb();
+      }
+      //reiniciar
+      this.reiniciar();
+    },
     addWeb(){
-      websiteRef.push(this.newWebSite);
+      let newObject = {
+        name : this.newWebSite.name,
+        autor : this.newWebSite.autor,
+        url : this.newWebSite.url
+      }
+      console.log(newObject);
+      websiteRef.push(newObject);
+      this.reiniciar();
+    },
+    editarWeb(tarea){
+      this.editar = true;
+      //primero rellanamos los campos
+      this.newWebSite['.key'] = tarea['.key'];
+      this.newWebSite.name = tarea.name;
+      this.newWebSite.autor = tarea.autor;
+      this.newWebSite.url = tarea.url;
+      console.log(this.newWebSite['.key']);
+    },
+    elimarWeb(tarea){
+      db.ref('websites/'+tarea['.key']).remove();
+    },
+    reiniciar(){
+      this.editar = false;
+      this.newWebSite['.key'] = '';
       this.newWebSite.name = '';
       this.newWebSite.autor = '';
       this.newWebSite.url = '';
-    },
-    mostrar(){
-      console.log(this.websitess);
     }
   }
 };
--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export default {
-    apiKey: "AIzaSyCgOd39ibQaOLGiRbSbSHpMGnT4iFqb-Kc",
-    authDomain: "vue-fire-cc176.firebaseapp.com",
-    databaseURL: "https://vue-fire-cc176.firebaseio.com",
-    projectId: "vue-fire-cc176",
-    storageBucket: "vue-fire-cc176.appspot.com",
-    messagingSenderId: "992032883944",
-    appId: "1:992032883944:web:c8c76961a9b6d341"
+  apiKey: "AIzaSyCgOd39ibQaOLGiRbSbSHpMGnT4iFqb-Kc",
+  authDomain: "vue-fire-cc176.firebaseapp.com",
+  databaseURL: "https://vue-fire-cc176.firebaseio.com",
+  projectId: "vue-fire-cc176",
+  storageBucket: "vue-fire-cc176.appspot.com",
+  messagingSenderId: "992032883944",
+  appId: "1:992032883944:web:c8c76961a9b6d341"
   }
